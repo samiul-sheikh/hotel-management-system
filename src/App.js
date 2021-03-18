c942d86eead785b899caf1ee80a7bdde53ebc927
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-19 04:45:33 +0600

    create private route to protect a path

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Book from './components/Book/Book';
 import Header from './components/Header/Header';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 export const UserContext = createContext();
 
 function App() {
     const [loggedInUser, setLoggedInUser] = useState({});
-    
+
     return (
         <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
             <p>Name: {loggedInUser.name}</p>
@@ -23,9 +24,9 @@ function App() {
                     <Route path="/login">
                         <Login />
                     </Route>
-                    <Route path="/book/:bedType">
+                    <PrivateRoute path="/book/:bedType">
                         <Book />
-                    </Route>
+                    </PrivateRoute>
                     <Route exact path="/">
                         <Home />
                     </Route>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
 import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router';
 
 const Login = () => {
 
@@ -11,6 +12,11 @@ const Login = () => {
     }
 
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+    const history = useHistory();
+    const location = useLocation();
+    const { from } = location.state || { from: { pathname: "/" } };
+
 
     const handleGoogleSignIn = () => {
         var provider = new firebase.auth.GoogleAuthProvider();
@@ -21,6 +27,7 @@ const Login = () => {
                 const { displayName, email } = result.user;
                 const signedInUser = { name: displayName, email: email }
                 setLoggedInUser(signedInUser);
+                history.replace(from);
             })
             .catch((error) => {
                 var errorCode = error.code;
