1ecb724728ca3d7f8a0161ab91510636cfc94409
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-19 03:29:08 +0600

    get simple google login authentication set

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,37 @@
 import React, { useContext } from 'react';
-
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
 
 const Login = () => {
+
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
+
+    const handleGoogleSignIn = () => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                var credential = result.credential;
+                var token = credential.accessToken;
+                var user = result.user;
+                console.log(token, user);
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+                console.log(errorCode, errorMessage, email, credential);
+            });
+    }
+
     return (
         <div>
             <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn}>sign in using Google</button>
         </div>
     );
 };
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAU5fcdHRlGTp7FtlWgWdgJ1v2vUklow54",
+    authDomain: "hotel-management-burj.firebaseapp.com",
+    projectId: "hotel-management-burj",
+    storageBucket: "hotel-management-burj.appspot.com",
+    messagingSenderId: "70318992853",
+    appId: "1:70318992853:web:38df0c3ebc8b3093b9a15c"
+};
+
+export default firebaseConfig;
