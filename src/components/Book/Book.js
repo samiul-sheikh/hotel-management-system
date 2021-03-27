174fce0b027ca5ad41ce047887dbc08036a67483
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-28 03:52:18 +0600

    send data to database and store in mongodb

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -31,8 +31,18 @@ const Book = () => {
         setSelectedDate(newDates);
     };
 
+    // create/send data to database
     const handleBooking = () => {
-
+        const newBooking = { ...loggedInUser, ...selectedDate };
+        fetch('http://localhost:8000/addBooking', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newBooking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
     }
 
     return (
